569a4ad9b57ff283545ee7613ac9479adb4590b4
med-shayan <shayn8313@gmail.com>
2025-12-20 00:48:04 +0100

    Add multiple medical terms to begriffe.js

--- begriffe.js
+++ begriffe.js
@@ -1,3 +1,12 @@
 const begriffe = [
-  { name: "Anämie", file: "anaemie.html" }
+  { name: "Anämie", file: "anaemie.html" },
+  { name: "Bradykardie", file: "bradykardie.html" },
+  { name: "Tachykardie", file: "tachykardie.html" },
+  { name: "Hypertonie", file: "hypertonie.html" },
+  { name: "Hypotonie", file: "hypotonie.html" },
+  { name: "Dyspnoe", file: "dyspnoe.html" },
+  { name: "Ödem", file: "oedem.html" },
+  { name: "Synkope", file: "synkope.html" },
+  { name: "Ikterus", file: "ikterus.html" },
+  { name: "Leukozytose", file: "leukozytose.html" }
 ];
