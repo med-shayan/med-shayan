56f3bd9a427f3c9f8c1a422218271812041d14bd
med-shayan <shayn8313@gmail.com>
2025-12-20 12:57:16 +0100

    Update begriffe.js

--- begriffe.js
+++ begriffe.js
@@ -1,12 +1,52 @@
 const begriffe = [
-  { name: "Anämie", file: "anaemie.html" },
-  { name: "Bradykardie", file: "bradykardie.html" },
-  { name: "Tachykardie", file: "tachykardie.html" },
-  { name: "Hypertonie", file: "hypertonie.html" },
-  { name: "Hypotonie", file: "hypotonie.html" },
-  { name: "Dyspnoe", file: "dyspnoe.html" },
-  { name: "Ödem", file: "oedem.html" },
-  { name: "Synkope", file: "synkope.html" },
-  { name: "Ikterus", file: "ikterus.html" },
-  { name: "Leukozytose", file: "leukozytose.html" }
+  {
+    name_de: "Anämie",
+    name_fa: "کم‌خونی",
+    file: "anaemie.html"
+  },
+  {
+    name_de: "Bradykardie",
+    name_fa: "برادی‌کاردی",
+    file: "bradykardie.html"
+  },
+  {
+    name_de: "Tachykardie",
+    name_fa: "تاکی‌کاردی",
+    file: "tachykardie.html"
+  },
+  {
+    name_de: "Hypertonie",
+    name_fa: "فشار خون بالا",
+    file: "hypertonie.html"
+  },
+  {
+    name_de: "Hypotonie",
+    name_fa: "فشار خون پایین",
+    file: "hypotonie.html"
+  },
+  {
+    name_de: "Dyspnoe",
+    name_fa: "تنگی نفس",
+    file: "dyspnoe.html"
+  },
+  {
+    name_de: "Ödem",
+    name_fa: "ادم",
+    file: "oedem.html"
+  },
+  {
+    name_de: "Synkope",
+    name_fa: "سنکوپ",
+    file: "synkope.html"
+  },
+  {
+    name_de: "Ikterus",
+    name_fa: "یرقان",
+    file: "ikterus.html"
+  },
+  {
+    name_de: "Leukozytose",
+    name_fa: "لکوسیتوز",
+    file: "leukozytose.html"
+  }
 ];
