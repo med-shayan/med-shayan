8b5442ce08c3f9189ef08481a60b53f7a3f65e99
med-shayan <shayn8313@gmail.com>
2025-12-20 14:39:32 +0100

    Update begriffe.js

--- begriffe.js
+++ begriffe.js
@@ -1,52 +1,81 @@
+const kategorien = {
+  haematologie: {
+    de: "Hämatologie",
+    fa: "هماتولوژی"
+  },
+  kardiologie: {
+    de: "Kardiologie",
+    fa: "قلب و عروق"
+  },
+  pneumologie: {
+    de: "Pneumologie",
+    fa: "ریه"
+  },
+  allgemein: {
+    de: "Allgemeinmedizin",
+    fa: "پزشکی عمومی"
+  }
+};
+
 const begriffe = [
   {
     name_de: "Anämie",
     name_fa: "کم‌خونی",
+    category: "haematologie",
     file: "anaemie.html"
+  },
+  {
+    name_de: "Leukozytose",
+    name_fa: "لکوسیتوز",
+    category: "haematologie",
+    file: "leukozytose.html"
   },
   {
     name_de: "Bradykardie",
     name_fa: "برادی‌کاردی",
+    category: "kardiologie",
     file: "bradykardie.html"
   },
   {
     name_de: "Tachykardie",
     name_fa: "تاکی‌کاردی",
+    category: "kardiologie",
     file: "tachykardie.html"
   },
   {
     name_de: "Hypertonie",
     name_fa: "فشار خون بالا",
+    category: "kardiologie",
     file: "hypertonie.html"
   },
   {
     name_de: "Hypotonie",
     name_fa: "فشار خون پایین",
+    category: "kardiologie",
     file: "hypotonie.html"
   },
   {
     name_de: "Dyspnoe",
     name_fa: "تنگی نفس",
+    category: "pneumologie",
     file: "dyspnoe.html"
   },
   {
     name_de: "Ödem",
     name_fa: "ادم",
+    category: "allgemein",
     file: "oedem.html"
   },
   {
     name_de: "Synkope",
     name_fa: "سنکوپ",
+    category: "allgemein",
     file: "synkope.html"
   },
   {
     name_de: "Ikterus",
     name_fa: "یرقان",
+    category: "allgemein",
     file: "ikterus.html"
-  },
-  {
-    name_de: "Leukozytose",
-    name_fa: "لکوسیتوز",
-    file: "leukozytose.html"
   }
 ];
